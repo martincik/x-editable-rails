b289ce25db739c0097fe0f270f94a768de11b2d7
Ladislav Martincik <ladislav.martincik@gmail.com>
2015-03-22 13:28:19 +0100

    Improving X-Editables to support formatters

--- lib/x-editable-rails/view_helpers.rb
+++ lib/x-editable-rails/view_helpers.rb
@@ -18,20 +18,21 @@ module X
         #       - enumerable shorthand ['Yes', 'No', 'Maybe'] becomes { 'Yes' => 'Yes', 'No' => 'No', 'Maybe' => 'Maybe' }
         #     classes: a Hash of classes to add based on the value (same format and shorthands as source)
         #   value: override the object's value
-        #
+        #   
         def editable(object, method, options = {})
           options = Configuration.method_options_for(object, method).deep_merge(options).with_indifferent_access
           # merge data attributes for backwards-compatibility
           options.merge! options.delete(:data){ Hash.new }
-
+          
           url     = options.delete(:url){ polymorphic_path(object) }
           object  = object.last if object.kind_of?(Array)
           value   = options.delete(:value){ object.send(method) }
+          value_formatter = options.delete(:value_formatter)
           source  = options[:source] ? format_source(options.delete(:source), value) : default_source_for(value)
           classes = format_source(options.delete(:classes), value)
           error   = options.delete(:e)
           html_options = options.delete(:html){ Hash.new }
-
+          
           if xeditable?(object)
             model   = object.class.name.split('::').last.underscore
             nid     = options.delete(:nid)
@@ -40,29 +41,30 @@ module X
               klass = nested ? object.class.const_get(nested.to_s.singularize.capitalize) : object.class
               klass.human_attribute_name(method)
             end
-
+            
             output_value = output_value_for(value)
             css_list = options.delete(:class).to_s.split(/\s+/).unshift('editable')
             css_list << classes[output_value] if classes
             type = options.delete(:type){ default_type_for(value) }
+            
             css   = css_list.compact.uniq.join(' ')
             tag   = options.delete(:tag){ 'span' }
             placeholder = options.delete(:placeholder){ title }
-
+            
             # any remaining options become data attributes
             data  = {
               type:   type,
-              model:  model,
-              name:   method,
+              model:  model, 
+              name:   method, 
               value:  ( type == 'wysihtml5' ? Base64.encode64(output_value) : output_value ), 
-              placeholder: placeholder,
-              classes: classes,
-              source: source,
-              url:    url,
-              nested: nested,
+              placeholder: placeholder, 
+              classes: classes, 
+              source: source, 
+              url:    url, 
+              nested: nested, 
               nid:    nid
             }.merge(options)
-
+            
             data.reject!{|_, value| value.nil?}
 
             html_options.update({
@@ -70,46 +72,53 @@ module X
               title: title,
               data: data
             })
-
+            
             content_tag tag, html_options do
-              safe_join(source_values_for(value, source), tag(:br)) unless %w(select checklist).include? data[:type]
+              safe_join(source_values_for(value.try(:html_safe), source), tag(:br)) unless %w(select checklist).include? data[:type]
             end
           else
-            error || safe_join(source_values_for(value, source), tag(:br))
+            error || safe_join(formated_source_values_for(value, value_formatter, source), tag(:br))
           end
         end
-
+        
         private
-
-        def output_value_for(value)
-          value = case value
+        
+        def output_value_for(value, formatter = nil)
+          case value
           when TrueClass
             '1'
           when FalseClass
             '0'
-          when NilClass
-            ''
           when Array
             value.map{|item| output_value_for item}.join(',')
           else
             value.to_s
           end
-
-          value
         end
 
+        def formated_source_values_for(value, formatter, source = nil)
+          values = source_values_for(value, source)
+          if formatter && formatter.is_a?(Proc)
+            values = values.map { |value| formatter.call(value) }
+          end
+          values
+        end
+        
         def source_values_for(value, source = nil)
           source ||= default_source_for value
-
           values = Array.wrap(value)
-
-          if source && ( source.first.is_a?(String) || source.kind_of?(Hash) )
-            values.map{|item| source[output_value_for item]}
+          
+          if source
+            if source.is_a? Proc
+              values.map { |item| source.call(item) }
+            elsif source.respond_to?(:[])
+              values.map { |item| source[item] }
+            end
           else
             values
           end
         end
-
+        
         def default_type_for(value)
           case value
           when TrueClass, FalseClass
@@ -120,14 +129,14 @@ module X
             'text'
           end
         end
-
+        
         def default_source_for(value)
           case value
           when TrueClass, FalseClass
             { '1' => 'Yes', '0' => 'No' }
           end
         end
-
+        
         # helper method that take some shorthand source definitions and reformats them
         def format_source(source, value)
           formatted_source = case value
@@ -142,10 +151,10 @@ module X
                 source
               end
             end
-
+          
           formatted_source || source
         end
-
+        
       end
     end
   end
